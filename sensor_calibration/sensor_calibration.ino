f8e0c186700099bb77dbd6b0b651c95fa909481f
jjtellez25 <jjt6@rice.edu>
2024-02-06 15:35:26 -0600

    sensor code

--- sensor_calibration/sensor_calibration.ino
+++ sensor_calibration/sensor_calibration.ino
@@ -1,7 +1,9 @@
-  const int trigPin = A;
-  const int echoPin = B;
+  const int trigPin = 11;
+  const int echoPin = 12;
   long duration;
-  int distanceCM, distanceInch;
+  int distanceCm, distanceInch;
+  const double R1 = 0.0343/2;
+  const double R2 = R1/2.54;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,7 +17,16 @@ void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(C);
-  digitalWrite(trig, LOW);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
+ 
+  distanceCm = duration * R1;
+  distanceInch = duration * R2;
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.print(" cm/");
+  Serial.print(distanceInch);
+  Serial.println(" in");
+  delay(1000);
 }

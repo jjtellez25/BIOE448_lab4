fc0b375d1233820a1aa027ba4915bb7a31b4d35f
jjtellez25 <jjt6@rice.edu>
2024-02-06 15:54:50 -0600

    Code including LED's

--- sensor_calibration/sensor_calibration.ino
+++ sensor_calibration/sensor_calibration.ino
@@ -1,14 +1,19 @@
-  const int trigPin = 11;
-  const int echoPin = 12;
-  long duration;
-  int distanceCm, distanceInch;
-  const double R1 = 0.0343/2;
-  const double R2 = R1/2.54;
+const int trigPin = 11;
+const int echoPin = 12;
+long duration;
+int distanceCm, distanceInch;
+const double R1 = 0.0343/2;
+const double R2 = R1/2.54;
+const int threshold = 3;
+const int ledPin = 2;
+const int ledPin2 = 6;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -29,4 +34,13 @@ void loop() {
   Serial.print(distanceInch);
   Serial.println(" in");
   delay(1000);
+
+  if (distanceInch < threshold) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, LOW);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, HIGH);
+  }
 }
